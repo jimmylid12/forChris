2e7482bc1c010db693d7bdfdcdd614da4d70563d
Jimmy Liddle <jameslid12@msn.com>
2018-09-20 15:54:58 +0100

    Initial Commit

---
+++ forChris/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  forChris
+//
+//  Created by James Liddle on 20/09/2018.
+//  Copyright © 2018 appCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    @IBOutlet var optionButtons: [UIButton]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    
+    
+    @IBAction func handleSelection(_ sender: UIButton)
+    {
+        optionButtons.forEach {(button) in
+            UIView.animate(withDuration: 0.3, animations:
+                {
+            button.isHidden = !button.isHidden
+                    self.view.layoutIfNeeded()
+    })
+    }
+    }
+    
+    enum Options: String
+    {
+        case health = "health"
+        case nutrition = "nutrtion"
+        case food = "food"
+    }
+    
+    @IBAction func optionTapped(_ sender: UIButton)
+    {
+        guard let title = sender.currentTitle, let option = Options(rawValue: title) else {
+            return
+        }
+        
+        switch option
+        {
+        case.health:
+            print ("health")
+        
+        case.nutrition:
+            print ("nutrtion")
+            
+        default:
+            print ("nutrition")
+            
+       
+            
+        }
+    }
+}
+
